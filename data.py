a4c313834c742489fd4792bec769002844a5f566
knownastazz <112016169+knownastazz@users.noreply.github.com>
2023-05-01 21:16:30 -0600

    Create data.py

---
+++ data.py
@@ -0,0 +1,10 @@
+enemies = [
+    "NAV" : [ 
+        "Abilities" : ["Bad intentions", "Emergency Tsunami", "Good Habits"],
+        "Drops" : [
+            ["Damage Potion", 0.25],
+            ["Health Potion", 0.25],
+            ["Damage Resistance Potion", 0.25]
+        ],
+        "Health" : 150
+    ]
