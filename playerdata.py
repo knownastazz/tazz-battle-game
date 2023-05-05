52fcca458b2c454535db1553187ab270942443fa
knownastazz <112016169+knownastazz@users.noreply.github.com>
2023-05-04 21:56:56 -0600

    Add files via upload

---
+++ playerdata.py
@@ -0,0 +1,24 @@
+data = {
+
+    "Rooms Beat" : [False, False, False, False, False],
+
+    "Weapons" : [
+        "Sword",
+    ],
+
+    "Abilities" : [
+        "Meditate",
+    ],
+
+    "Consumables" : [
+        ["Damage Potion", 1],
+        ["Health Potion", 1],
+        ["Damage Resistance Potion", 1],
+        ["GOLDEN APPLE", 0],
+    ],
+
+    "weapon" : "Sword",
+
+    "ability" : "Meditate"
+
+}
